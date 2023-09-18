f59336f0b9f0e0f24485b42a8334839a4d34c8cc
Nathan Lambertson <lambertones@gmail.com>
2023-09-18 14:02:42 -0500

    add an else

--- conditionals.rb
+++ conditionals.rb
@@ -1,4 +1,7 @@
 password = gets.chomp
 if password.length >= 8 && password.index("#") != nil
     puts "Got it, your password is #{password}"
+else
+    puts "#{password} is not a valid password!"
 end
+
